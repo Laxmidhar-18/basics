a5ebf9200bfe40b163a0e8fbf8bc407418f55fdf
Laxmidhar-18 <lbiswal468@gmail.com>
2026-07-31 21:40:52 +0530

    Add files via upload

---
+++ v_shape_star.java
@@ -0,0 +1,16 @@
+public class v_shape_star {
+    public static void main(String[] args) {
+      int n = 5;
+      for(int i = 1; i <= n; i++){
+          for(int j=1; j <= 2*n; j++){
+              if ((i==j) || (i+j == 2*n)){
+                  System.out.print("*");
+              }else{
+                  System.out.print(" ");
+              }
+          }
+          System.out.println();
+      }
+
+    }
+}
