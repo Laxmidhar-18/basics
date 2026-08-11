d3e6ddfe80f9f729d1bf09a6593b76e2b9e70eee
Laxmidhar-18 <lbiswal468@gmail.com>
2026-08-11 22:51:33 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,19 @@
+public class name {
+    public static void main(String[] args){
+        int year=1900;
+        if(year%100 ==0){
+            if(year%400== 0){
+                System.out.println(year+ "year is leap year");
+            }else{
+                System.out.println(year + "year is ont leap year");
+            }
+
+        }else{
+            if(year%4 == 0){
+                System.out.println(year+ "year is leap year");
+            }else{
+                System.out.println(year + "year is ont leap year");
+            }
+        }
+    }
+}
