60fb147639846073286ee4740bf3aa629f836ee0
Laxmidhar Biswal <lbiswal468@gmail.com>
2026-08-20 15:02:58 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class reverse
+{
+    public static void main(String[] args)
+    {
+       int arr[] = {1,3,4,5,6};
+        for (int i = 0; i <=arr.length-1; i--) {
+            System.out.print(Arrays.toString(arr));
+        }
+    }
+}
