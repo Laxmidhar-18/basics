08342d561bedf580b37c1c9355021d5a664319ea
Laxmidhar Biswal <lbiswal468@gmail.com>
2026-08-21 12:26:41 +0530

    Add files via upload

---
+++ operator_me.java
@@ -0,0 +1,9 @@
+public class operator_me {
+    public static void main(String[] args) {
+//        System.out.println(10+20+"30");
+//        System.out.println("10"+20+30);
+//        System.out.println(10+"20"+30);
+//        System.out.println();
+
+    }
+}
