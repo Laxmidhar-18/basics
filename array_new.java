2a5385138a87e2c4194c64ede0536a79f0c11ee3
Laxmidhar Biswal <lbiswal468@gmail.com>
2026-08-22 20:20:46 +0530

    Add files via upload

---
+++ array_new.java
@@ -0,0 +1,20 @@
+public class array_new {
+    public static void main(String[] args) {
+//        int age = 32;
+           int arr[] = new int[780];
+           arr[0] = 10;
+           arr[1] = 30;
+           arr[2] = 40;
+           arr[3] = 50;
+           arr[4] = 60;
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+//        System.out.println(arr[4]);
+        for (int i = 0; i < arr.length;i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
