7a807f8042b7e0a1ad0137aeea75ceb54b1da903
Laxmidhar Biswal <lbiswal468@gmail.com>
2026-08-23 19:44:48 +0530

    Add files via upload

---
+++ rotate_java.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class rotate_java {
+    public static void main(String[] args) {
+        int arr[] = {1,23,4,5,6};
+////       int n = arr.length, k = 2;
+////        for (int j = 0; j<= k ; j++) {
+////
+////            int temp = arr[0];
+////            for (int i = 1; i < n; i++) {
+////                arr[i-1] = arr[i];
+////            }
+////            arr[n-1] = temp;
+    }
+
+}
