4c3e34dbd08ea2793f1f652d7dc561261a170daf
Laxmidhar Biswal <lbiswal468@gmail.com>
2026-08-26 22:23:50 +0530

    Add files via upload

---
+++ soritng_java.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class soritng_java {
+    public static void main(String[] args) {
+      int arr[] = {3,5,2,4,6,9,1};
+      bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubblesort(int arr[]){
+        int n = arr.length;
+        for (int i = 0; i <n-1 ; i++) {
+            for (int j = 0; j <n-1 ; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
